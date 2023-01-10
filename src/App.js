675ac46f712e2881d513dc2d4041a10ac42bcf4d
aek20 <azozek1420@gmail.com>
2023-01-10 10:54:06 -1000

    10 jan

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from "./components/navbar/Navbar.js";
 import LandingPage from './components/landingPage/LandingPage';
+import FlutterProject from './components/flutter/FlutterProject';
 function App() {
   return (
     <div className="App">
 <Navbar/>
 <LandingPage />
+<FlutterProject />
     </div>
   );
 }
--- src/components/flutter/FlutterProject.js
+++ src/components/flutter/FlutterProject.js
@@ -1,24 +1,37 @@
 import React from 'react'
-import './landing.css'
 import {Container , Row ,Col ,Button} from 'react-bootstrap'
-import 'animate.css';
-import img from '../../img/programmer.png'
-export default function LandingPage() {
+export default function FlutterProject() {
   return (
-    <div>
-
-<Container>
+ 
+        <div className='projects'>
+            <h1>Flutter and React Native</h1>
+    <a href="https://www.flaticon.com/free-icons/coding" title="coding icons">Coding icons created by prettycons - Flaticon</a>
+    <a href="https://www.flaticon.com/free-icons/coding" title="coding icons">Coding icons created by Freepik - Flaticon</a>
+    <Container>
       <Row>
-        <Col>
-        <h1 class="animate__animated animate__swing animate__zoomIn"> hi i'm a full-stack web developer</h1>
-        <Button variant="secondary" className='github'> <a href='https://github.com/aek20' target="_blank">GitHub account</a></Button>
+        <Col xs><h1>chatbot builder </h1>
+        
+        <h2>my GRADUAQTE PROJECT i work now with my supervisor to publish it</h2>
         </Col>
-        <Col className='sec-col'> 
-     <img className="dev-icon" src={img}/>
+        <Col xs><h1>Book Rating website </h1>
+        
+        <h2>my GRADUAQTE PROJECT i work now with my supervisor to publish it</h2></Col>
+        <Col xs><h1>World Cup Game</h1>
+        
+        <h2> questions about world cup 
+            using flutter with firebase
+        </h2>
         </Col>
       </Row>
     </Container>
-    
+
+    <iframe width="420" height="315"
+src="https://www.youtube.com/embed/tgbNymZ7vqY">
+</iframe>
+
+    </div>
+  )
+}
     <div className='projects'>
     <a href="https://www.flaticon.com/free-icons/coding" title="coding icons">Coding icons created by prettycons - Flaticon</a>
     <a href="https://www.flaticon.com/free-icons/coding" title="coding icons">Coding icons created by Freepik - Flaticon</a>
@@ -40,7 +53,3 @@ export default function LandingPage() {
 
 
     </div>
-
-    </div>
-  )
-}
